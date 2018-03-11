9dbd409a922da2badf6978ef83137498658913b0
Ian Tracey <ian.tracey@latitudeengineering.com>
2018-03-10 18:08:42 -0800

    Add vote update cache

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -16,9 +16,25 @@ class LinkList extends Component {
     const linksToRender = this.props.feedQuery.feed.links;
 
     return (
-      <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+      <div>
+        {linksToRender.map((link, index) => (
+          <Link
+            key={link.id}
+            updateStoreAfterVote={this._updateCacheAfterVote}
+            index={index}
+            link={link}
+          />
+        ))}
+      </div>
     );
   }
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
 }
 
 const FEED_QUERY = gql`
@@ -29,6 +45,16 @@ const FEED_QUERY = gql`
         createdAt
         description
         url
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
