4b9aa3f2d6990027705e8a033cedd90f9bd936f6
Ian Tracey <ian.tracey@latitudeengineering.com>
2018-03-10 17:20:49 -0800

    Create Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import LinkList from './components/LinkList';
+import CreateLink from './components/CreateLink';
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return (
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
+    );
   }
 }
 
