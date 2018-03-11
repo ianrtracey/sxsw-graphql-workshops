e217405ae5cdde68f39dd368b2791af81767c14f
Ian Tracey <ian.tracey@latitudeengineering.com>
2018-03-10 17:12:44 -0800

    link query

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import Link from './Link';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API  ',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ];
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>;
+    }
+
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>;
+    }
+
+    const linksToRender = this.props.feedQuery.feed.links;
 
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
@@ -22,4 +21,19 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList;
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        description
+        url
+      }
+    }
+  }
+`;
+
+export default graphql(FEED_QUERY, {
+  name: 'feedQuery',
+})(LinkList);
